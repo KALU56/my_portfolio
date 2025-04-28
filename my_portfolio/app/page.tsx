7f434f768ed076eaecd96e00a81dfb9e071a2349
KALU56 <kalkidanbehailu55@gmail.com>
2025-04-28 18:36:04 +0300

    i do the inisiaize the portfolio

---
+++ my_portfolio/app/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/ModeToggle";
+function Homepage() {
+  return (
+    <div>
+     <ModeToggle />
+     <div className="bg-primary">
+      <p className="text-white text-center text-2xl font-bold">what is thooo</p>
+
+     </div>
+    </div>
+  );
+}
+export default Homepage;
